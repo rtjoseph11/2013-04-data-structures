5a113b14b7c51c9ce208a496cb7f8cd46c5dc507
Hack Reactor Students <github@hackreactor.com>
2013-04-29 16:09:28 -0700

    made psueoclassical queue look cleaner

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,31 +1,31 @@
 var Queue = function(){
   // Use an object with numeric keys to store values
-  this.storage = {};
-  this.queueSize=0; // Hint: set an initial value here
+  this._storage = {};
+  this._queueSize = 0; // Hint: set an initial value here
 };
 
 Queue.prototype.add = function(value){
-    this.storage[this.queueSize] = value;
-    this.queueSize++;
-  };
+  this.storage[this.queueSize] = value;
+  this.queueSize++;
+};
 Queue.prototype.remove = function(){
-    if (this.queueSize > 0){
-      var storage = this.storage;
-      var removedItem = storage[0];
-      delete storage[0];
-      this.queueSize--;
-      for (var key in storage){
-        if(storage.hasOwnProperty(key)){
-          storage[key-1] = storage[key];
-        }
+  if (this.queueSize > 0){
+    var storage = this.storage;
+    var removedItem = storage[0];
+    delete storage[0];
+    this.queueSize--;
+    for (var key in storage){
+      if(storage.hasOwnProperty(key)){
+        storage[key-1] = storage[key];
       }
-      delete storage[this.queueSize];
-      return removedItem;
     }
-  };
+    delete storage[this.queueSize];
+    return removedItem;
+  }
+};
 Queue.prototype.size = function(){
-    return this.queueSize;
-  };
+  return this.queueSize;
+};
 Queue.prototype.getStorage = function() {
-    return this.storage;
-  };
+  return this.storage;
+};
