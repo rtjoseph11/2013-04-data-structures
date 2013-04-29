cc3b4cc184abfb48b99eb25c07d1d07b5d46deaf
Hack Reactor Students <github@hackreactor.com>
2013-04-29 15:04:53 -0700

    Implemented prototypal queue and stack

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,1 +1,41 @@
-// Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var Queue = function(){
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  var queueSize=0; // Hint: set an initial value here
+
+  // Implement the methods below
+  var queue = Object.create(queueMethods);
+  queue.queueSize = queueSize;
+  queue.storage = storage;
+  return queue;
+};
+
+var queueMethods = {
+  add: function(value){
+    this.storage[this.queueSize] = value;
+    this.queueSize++;
+  },
+  remove: function(){
+    if (this.queueSize > 0){
+      var storage = this.storage;
+      var removedItem = storage[0];
+      delete storage[0];
+      this.queueSize--;
+      for (var key in storage){
+        if(storage.hasOwnProperty(key)){
+          storage[key-1] = storage[key];
+        }
+      }
+      delete storage[this.queueSize];
+      return removedItem;
+    }
+  },
+  size: function(){
+    return this.queueSize;
+  },
+  getStorage : function() {
+    return this.storage;
+  }
+};
+// Hey! Copy your code from src/functional/queue.js and paste it here
--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,1 +1,35 @@
-// Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var Stack = function(){
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  var stackSize = 0; // Hint: set an initial value here
+  var stack = Object.create(stackMethods);
+  _.extend(stack,{
+    stackSize: stackSize,
+    storage: storage
+  });
+  return stack;
+  // Implement the methods below
+};
+
+var stackMethods = {
+  add : function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+  remove : function(){
+    if (this.stackSize > 0){
+    this.stackSize--;
+    var returnObject = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return returnObject;
+    }
+  },
+  size : function(){
+    return this.stackSize;
+  },
+  getStorage : function(){
+    return this.storage;
+  }
+};
+// Hey! Copy your code from src/functional/stack.js and paste it here
