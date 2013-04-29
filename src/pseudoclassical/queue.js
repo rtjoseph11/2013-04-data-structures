734241d9f33bda57f526b20e9b2b4a3ab5a9cb4d
Hack Reactor Students <github@hackreactor.com>
2013-04-29 15:17:23 -0700

    Implemented pseudoclassical queue and stack

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,1 +1,31 @@
-// Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.queueSize=0; // Hint: set an initial value here
+};
+
+Queue.prototype.add = function(value){
+    this.storage[this.queueSize] = value;
+    this.queueSize++;
+  };
+Queue.prototype.remove = function(){
+    if (this.queueSize > 0){
+      var storage = this.storage;
+      var removedItem = storage[0];
+      delete storage[0];
+      this.queueSize--;
+      for (var key in storage){
+        if(storage.hasOwnProperty(key)){
+          storage[key-1] = storage[key];
+        }
+      }
+      delete storage[this.queueSize];
+      return removedItem;
+    }
+  };
+Queue.prototype.size = function(){
+    return this.queueSize;
+  };
+Queue.prototype.getStorage = function() {
+    return this.storage;
+  };
--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,1 +1,25 @@
-// Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.stackSize = 0; // Hint: set an initial value here
+  // Implement the methods below
+};
+
+Stack.prototype.add = function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+};
+Stack.prototype.remove = function(){
+    if (this.stackSize > 0){
+    this.stackSize--;
+    var returnObject = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return returnObject;
+    }
+};
+Stack.prototype.size = function(){
+    return this.stackSize;
+};
+Stack.prototype.getStorage = function(){
+    return this.storage;
+};
